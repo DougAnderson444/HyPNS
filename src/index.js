142fdf0a70c7c755ac5a7f927ec9a4bb4ae877fb
DougA <douganderson444@gmail.com>
2020-10-16 14:34:04 -0400

    apply standard lint format

--- src/index.js
+++ src/index.js
@@ -1,91 +1,96 @@
-const Corestore = require("corestore");
-const SwarmNetworker = require("@corestore/networker");
+/* eslint-disable no-unused-expressions */
+const Corestore = require('corestore')
+const SwarmNetworker = require('@corestore/networker')
 
-const Multifeed = require("hypermultifeed");
-const MultifeedNetworker = require("hypermultifeed/networker");
+const Multifeed = require('hypermultifeed')
+const MultifeedNetworker = require('hypermultifeed/networker')
 
-var kappa = require("kappa-core");
-var list = require("@DougAnderson444/kappa-view-list");
-var memdb = require("level-mem");
+var kappa = require('kappa-core')
+var list = require('@DougAnderson444/kappa-view-list')
+var memdb = require('level-mem')
 
-var RAM = require("random-access-memory");
-const RAI = require("@DougAnderson444/random-access-idb");
+var RAM = require('random-access-memory')
+const RAI = require('@DougAnderson444/random-access-idb')
 
-const pify = require("pify"); // promisify
-var EventEmitter = require("events").EventEmitter;
-const once = require("events.once"); // polyfill for nodejs events.once in the browser
+const pify = require('pify') // promisify
+var EventEmitter = require('events').EventEmitter
+// const once = require('events.once') // polyfill for nodejs events.once in the browser
 
-const hcrypto = require("hypercore-crypto");
-const sodium = require("sodium-universal");
+const hcrypto = require('hypercore-crypto')
+const sodium = require('sodium-universal')
 
-let isBrowser = process.title === "browser";
+const isBrowser = process.title === 'browser'
 
 class HyPNS {
-  constructor(opts) {
+  constructor (opts) {
     this._storage =
-      opts.persist === false || !opts.storage ? RAM : getNewStorage();
-    this.store = new Corestore(this._storage);
-    this.swarmNetworker = new SwarmNetworker(this.store);
-    this.network = new MultifeedNetworker(this.swarmNetworker);
+      opts.persist === false || !opts.storage ? RAM : getNewStorage()
+    this.store = new Corestore(this._storage)
+    this.swarmNetworker = new SwarmNetworker(this.store)
+    this.network = new MultifeedNetworker(this.swarmNetworker)
 
     // handle shutdown gracefully
     const closeHandler = async () => {
-      console.log("Shutting down...");
-      await this.close();
-      process.exit();
-    };
+      console.log('Shutting down...')
+      await this.close()
+      process.exit()
+    }
 
-    process.on("SIGINT", closeHandler);
-    process.on("SIGTERM", closeHandler);
+    process.on('SIGINT', closeHandler)
+    process.on('SIGTERM', closeHandler)
   }
 
-  async open(opts) {
-    const instance = new HyPNSInstance({ ...opts, ...this });
-    await instance.ready;
-    return instance;
+  async open (opts) {
+    const instance = new HyPNSInstance({ ...opts, ...this })
+    await instance.ready
+    return instance
   }
-  async close() {
+
+  async close () {
     this.store.close()
-    await this.swarmNetworker.close(); //Shut down the swarm networker.
+    await this.swarmNetworker.close() // Shut down the swarm networker.
   }
 }
 
 class HyPNSInstance {
-  constructor(opts) {
+  constructor (opts) {
     if (
       !opts.keypair ||
       !opts.keypair.publicKey ||
       !opts.keypair.publicKey === null ||
-      Buffer.byteLength(opts.keypair.publicKey, "hex") !==
-        sodium.crypto_sign_PUBLICKEYBYTES
+      Buffer.byteLength(opts.keypair.publicKey, 'hex') !==
+      sodium.crypto_sign_PUBLICKEYBYTES
     ) {
       // make new keypair for them
-      opts.keypair = hcrypto.keyPair();
+      opts.keypair = hcrypto.keyPair()
     }
-    this._keypair = opts.keypair;
-    this.store = opts.store;
-    this.network = opts.network;
-    this.beacon = new EventEmitter();
-    this.multi;
-    this.core;
-    this.latest;
-    this.writable;
+    this._keypair = opts.keypair
+    this.key = this._keypair.publicKey
+    // this.publicKey = this._keypair.publicKey.toString('hex')
+    this.store = opts.store
+    this.network = opts.network
+    this.beacon = new EventEmitter()
+    // eslint-disable-next-line no-unused-expressions
+    this.multi
+    this.core
+    this.latest
+    this.writable
     this.publish = () => {
-      return null;
-    };
+      return null
+    }
 
-    this.ready = this.open();
+    this.ready = this.open()
   }
 
-  async open() {
-    return new Promise(async (resolve, reject) => {
-      var self = this;
+  async open () {
+    return new Promise((resolve, reject) => {
+      var self = this
 
       this.multi = new Multifeed(this.store, {
         rootKey: this._keypair.publicKey,
-        valueEncoding: "json",
-      });
-      this.network.swarm(this.multi);
+        valueEncoding: 'json'
+      })
+      this.network.swarm(this.multi)
 
       /**
        * KAPPA VIEWS of the multifeed
@@ -96,119 +101,121 @@ class HyPNSInstance {
           msg.value &&
           msg.value.text &&
           msg.value.timestamp &&
-          typeof msg.value.timestamp === "string" &&
+          typeof msg.value.timestamp === 'string' &&
           this.verify(
-            msg.value.text + " " + msg.value.timestamp,
+            msg.value.text + ' ' + msg.value.timestamp,
             msg.value.signature
-          );
+          )
         if (valid) {
           // sort on the 'timestamp' field
-          next(null, [msg.value.timestamp]);
+          next(null, [msg.value.timestamp])
         } else {
-          next();
+          next()
         }
-      });
+      })
 
-      this.core = kappa(this.store, { multifeed: this.multi }); // store not used since we pass in a multifeed
+      this.core = kappa(this.store, { multifeed: this.multi }) // store not used since we pass in a multifeed
 
       // read
-      this.core.use("pointer", timestampView);
+      this.core.use('pointer', timestampView)
       this.core.api.pointer.tail(1, (msgs) => {
-        this.latest = msgs[0].value;
-        this.beacon.emit("update", msgs[0].value);
-      });
+        this.latest = msgs[0].value
+        this.beacon.emit('update', msgs[0].value)
+      })
 
-      this.core.ready("pointer", () => {
+      this.core.ready('pointer', () => {
         if (this.writeEnabled()) {
           // writer
-          this.core.writer("kappa-local", (err, feed) => {
-            if (err) reject(err);
+          this.core.writer('kappa-local', (err, feed) => {
+            if (err) reject(err)
 
-            function pub(data) {
-              const timestamp = new Date().toISOString();
+            function pub (data) {
+              const timestamp = new Date().toISOString()
               const signature = hcrypto.sign(
-                Buffer.from(data.text + " " + timestamp, "utf8"),
-                Buffer.from(self._keypair.secretKey, "hex") // has to be self._ so that .bind doesn't replace it with feed
-              );
-              let objPub = {
+                Buffer.from(data.text + ' ' + timestamp, 'utf8'),
+                Buffer.from(self._keypair.secretKey, 'hex') // has to be self._ so that .bind doesn't replace it with feed
+              )
+              const objPub = {
                 ...data,
-                signature: signature.toString("hex"),
-                timestamp,
-              };
-              this.append(objPub); // this gets bound to the object's kappa-local feed above
-              return objPub;
+                signature: signature.toString('hex'),
+                timestamp
+              }
+              this.append(objPub) // this gets bound to the object's kappa-local feed above
+              return objPub
             }
 
-            this.publish = pub.bind(feed); // bind feed to this in pub()
+            this.publish = pub.bind(feed) // bind feed to this in pub()
 
-            feed.ready(() => resolve(true));
-          });
+            feed.ready(() => {
+              this.writable = true
+              resolve(true)
+            })
+          })
         } else {
-          resolve(true);
+          resolve(true)
         }
-      });
-    });
+      })
+    })
   }
 
-  async close() {
-    this.multi.close(); // closes individual multifeed
+  async close () {
+    this.multi.close() // closes individual multifeed
   }
 
-  writeEnabled() {
-    if (!this._keypair.secretKey) return false;
+  writeEnabled () {
+    if (!this._keypair.secretKey) return false
     if (
-      Buffer.byteLength(this._keypair.secretKey, "hex") !==
+      Buffer.byteLength(this._keypair.secretKey, 'hex') !==
       sodium.crypto_sign_SECRETKEYBYTES
-    )
-      return false;
+    ) { return false }
 
-    const message = Buffer.from("any msg will do", "utf8");
+    const message = Buffer.from('any msg will do', 'utf8')
 
     // sign something with this secretKey
-    const signature = Buffer.allocUnsafe(sodium.crypto_sign_BYTES);
+    const signature = Buffer.allocUnsafe(sodium.crypto_sign_BYTES)
     sodium.crypto_sign_detached(
       signature,
       message,
-      Buffer.from(this._keypair.secretKey, "hex")
-    );
+      Buffer.from(this._keypair.secretKey, 'hex')
+    )
 
     // verify the signature matches the given public key
     return sodium.crypto_sign_verify_detached(
       signature,
       message,
-      Buffer.from(this._keypair.publicKey, "hex")
-    );
+      Buffer.from(this._keypair.publicKey, 'hex')
+    )
   }
 
-  get publicKey() {
-    return this._keypair.publicKey.toString("hex");
+  get publicKey () {
+    return this._keypair.publicKey.toString('hex')
   }
 
-  async readLatest() {
+  async readLatest () {
     // get the last saved entry
     const msgs = await pify(this.core.api.pointer.read)({
       limit: 1,
-      reverse: true,
-    });
+      reverse: true
+    })
 
     return msgs && msgs.length > 0 && msgs[0].value && msgs[0].value.text
       ? msgs[0].value.text
-      : null;
+      : null
   }
 
-  verify(message, signature) {
+  verify (message, signature) {
     // verify(message, signature, publicKey) // verify the signature of this value matches the public key under which it was published
     return hcrypto.verify(
-      Buffer.from(message, "utf8"),
-      Buffer.from(signature, "hex"),
-      Buffer.from(this._keypair.publicKey, "hex")
-    );
+      Buffer.from(message, 'utf8'),
+      Buffer.from(signature, 'hex'),
+      Buffer.from(this._keypair.publicKey, 'hex')
+    )
   }
 }
 
-module.exports = HyPNS;
+module.exports = HyPNS
 
-function getNewStorage() {
-  if (isBrowser) return RAI("hypns");
-  else return require("tmp").dirSync({ prefix: "hypns-" }).name;
+function getNewStorage () {
+  if (isBrowser) return RAI('hypns')
+  else return require('tmp').dirSync({ prefix: 'hypns-' }).name
 }
--- test/basic.js
+++ test/basic.js
@@ -1,163 +1,180 @@
-const chai = require("chai");
-var chaiAsPromised = require("chai-as-promised");
-chai.use(chaiAsPromised);
-var expect = chai.expect;
+/* eslint-disable no-unused-expressions */
+/* eslint-disable no-undef */
+const chai = require('chai')
+var chaiAsPromised = require('chai-as-promised')
+chai.use(chaiAsPromised)
+var expect = chai.expect
 
-const HyPNS = require("../src");
-const once = require("events.once"); // polyfill for nodejs events.once in the browser
+const HyPNS = require('../src')
+const once = require('events.once') // polyfill for nodejs events.once in the browser
 
-const helper = require("./lib");
+const helper = require('./lib')
 
 const mockPublicKey =
-  "dee2fc9db57f409cfa5edea42aa40790f3c1b314e3630a04f25b75ad42b71835";
+  'dee2fc9db57f409cfa5edea42aa40790f3c1b314e3630a04f25b75ad42b71835'
 const mockPrivateKey =
-  "1e9813baf16eb415a61a56693b037d5aec294279b35a814aff239a0c61f71d3bdee2fc9db57f409cfa5edea42aa40790f3c1b314e3630a04f25b75ad42b71835";
+  '1e9813baf16eb415a61a56693b037d5aec294279b35a814aff239a0c61f71d3bdee2fc9db57f409cfa5edea42aa40790f3c1b314e3630a04f25b75ad42b71835'
 const mockKeypair = {
   publicKey: mockPublicKey,
-  secretKey: mockPrivateKey,
-};
+  secretKey: mockPrivateKey
+}
 const mockObjPub = {
-  text: "Some test data to publish " + new Date().toISOString(),
-  type: "chat-message",
-  nickname: "cat-lover",
-};
+  text: 'Some test data to publish ' + new Date().toISOString(),
+  type: 'chat-message',
+  nickname: 'cat-lover'
+}
 const mockObjPub2 = {
-  text: "Some other test data to publish " + new Date().toISOString(),
-  type: "chat-message",
-  nickname: "cat-lover",
-};
+  text: 'Some other test data to publish ' + new Date().toISOString(),
+  type: 'chat-message',
+  nickname: 'cat-lover'
+}
 
-describe("Persist: false", async function () {
-  var myNode = new HyPNS({ persist: false }); // pass in optional Corestore and networker
-  var instance;
-  var readerOnly;
+process.on('warning', (warning) => {
+  // console.warn(warning.name);    // Print the warning name
+  // console.warn(warning.message); // Print the warning message
+  console.warn(warning.stack) // Print the stack trace
+})
+
+describe('Persist: false', async function () {
+  var myNode = new HyPNS({ persist: false }) // pass in optional Corestore and networker
+  var instance
+  var readerOnly
   before(async function () {
     // runs once before the first test in this block
-    instance = await myNode.open({ keypair: mockKeypair });
-    readerOnly = await myNode.open({ keypair: { publicKey: mockPublicKey } });
-  });
+    instance = await myNode.open({ keypair: mockKeypair })
+    readerOnly = await myNode.open({ keypair: { publicKey: mockPublicKey } })
+  })
+
+  afterEach(function () {
+    // runs after each test in this block
+    // console.log('myNode corestore contents',myNode.store.list())
+    // console.log('myNode', myNode.store)
+  })
 
   after(function (done) {
     // runs once after the last test in this block
-    this.timeout(30000); // takes time to close all the connections
+    this.timeout(30000) // takes time to close all the connections
     myNode
       .close()
       .catch((err) => console.error(err))
-      .then(done);
-  });
-  describe("Writer", async function () {
-    it("should create a HyPNS instance", async function () {
-      expect(instance.publicKey).to.equal(mockPublicKey);
-    });
+      .then(done)
+  })
+  describe('Writer', async function () {
+    it('should create a HyPNS instance', async function () {
+      expect(instance.publicKey).to.equal(mockPublicKey)
+    })
 
-    it("should start with empty latest value", function (done) {
+    it('should start with empty latest value', function (done) {
       instance
         .readLatest()
         .catch((err) => console.error(err))
         .then((val) => {
-          expect(val).to.equal(null);
-          done();
-        });
-    });
+          expect(val).to.equal(null)
+          done()
+        })
+    })
 
-    it("should be writeEnabled", async function () {
-      expect(instance.writeEnabled()).to.be.true;
-    });
+    it('should be writeEnabled', function () {
+      expect(instance.writeEnabled()).to.be.true
+    })
 
-    it("should publish and emit the same", async function () {
-      const retVal = instance.publish(mockObjPub);
-      expect(retVal.text).to.equal(mockObjPub.text);
+    it('should publish and emit the same', async function () {
+      const retVal = instance.publish(mockObjPub)
+      expect(retVal.text).to.equal(mockObjPub.text)
 
-      const [val] = await once(instance.beacon, "update");
-      expect(val.text).to.equal(mockObjPub.text);
-      expect(val).to.have.property("signature");
-    });
+      const [val] = await once(instance.beacon, 'update')
+      expect(val.text).to.equal(mockObjPub.text)
+      expect(val).to.have.property('signature')
+    })
 
-    it("should publish a second value and emit the same", async function () {
-      const retVal = instance.publish(mockObjPub2);
-      const [val] = await once(instance.beacon, "update");
-      expect(retVal.text).to.equal(mockObjPub2.text);
-      expect(val.text).to.equal(mockObjPub2.text);
-    });
+    it('should publish a second value and emit the same', async function () {
+      const retVal = instance.publish(mockObjPub2)
+      const [val] = await once(instance.beacon, 'update')
+      expect(retVal.text).to.equal(mockObjPub2.text)
+      expect(val.text).to.equal(mockObjPub2.text)
+    })
 
-    it("should ignore entries without a timestamp", function (done) {
-      expect(instance.core.feeds().length).to.equal(1);
+    it('should ignore entries without a timestamp', function (done) {
+      expect(instance.core.feeds().length).to.equal(1)
       // saved from another library to this publicKey
       helper.anotherWriter(instance.core, async () => {
-        expect(instance.core.feeds().length).to.equal(2);
-        var totalEntries = 0;
+        expect(instance.core.feeds().length).to.equal(2)
+        var totalEntries = 0
         instance.core.feeds().forEach((f) => {
-          totalEntries += f.length;
-        });
-        expect(totalEntries).to.equal(3);
+          totalEntries += f.length
+        })
+        expect(totalEntries).to.equal(3)
 
         instance
           .readLatest()
           .catch((err) => console.error(err))
           .then((val) => {
-            expect(val).to.equal(mockObjPub2.text);
-            done();
-          });
-      });
-    });
-  });
+            expect(val).to.equal(mockObjPub2.text)
+            done()
+          })
+      })
+    })
+  })
 
-  describe("Reader", async function () {
-    it("should be read only if only passed Public key and no private key", async function () {
-      expect(readerOnly.writeEnabled()).to.be.false;
+  describe('Reader', async function () {
+    it('should be read only if only passed Public key and no private key', async function () {
+      expect(readerOnly.writeEnabled()).to.be.false
       // need to wait until peers are confirmed as conencted before read
-      this.timeout(1000);
+      this.timeout(1000)
       try {
-        var val = await readerOnly.readLatest();
-        expect(val).to.equal(mockObjPub2.text);
+        var val = await readerOnly.readLatest()
+        expect(val).to.equal(mockObjPub2.text)
       } catch (error) {
-        (error) => console.error(error);
+        (error) => console.error(error)
       }
-    });
+    })
 
-    it("should ignore readonly publish command", function () {
-      expect(readerOnly.publish({ text: "foo" })).to.equal(null);
-    });
-  });
+    it('should ignore readonly publish command', function () {
+      expect(readerOnly.publish({ text: 'foo' })).to.equal(null)
+    })
+  })
 
-  it("should create new key if no public key is passed", async function () {
-    const keyGen = await myNode.open();
-    expect(keyGen).to.have.property("publicKey");
-  });
+  it('should create new key if no public key is passed', async function () {
+    const keyGen = await myNode.open()
+    // console.log('keyGen', keyGen.store)
 
-  it("should not be writeEnabled if bad secret key is passed", async function () {
+    expect(keyGen).to.have.property('publicKey')
+  })
+
+  it('should not be writeEnabled if bad secret key is passed', async function () {
     var badSecretKey = await myNode.open({
-      keypair: { publicKey: mockPublicKey, secretKey: "foo" },
-    });
+      keypair: { publicKey: mockPublicKey, secretKey: 'foo' }
+    })
+    // console.log('badSecretKey', badSecretKey.store)
 
-    expect(badSecretKey.writeEnabled()).to.be.false;
-  });
-});
+    expect(badSecretKey.writeEnabled()).to.be.false
+  })
+})
 
-describe("Persist:true", function () {
-
-  var persistNode = new HyPNS({ persist: false }); // pass in optional Corestore and networker
-  var persistH;
+describe('Persist:true', function () {
+  var persistNode = new HyPNS({ persist: false }) // pass in optional Corestore and networker
+  var persistH
   before(async function () {
     // runs once before the first test in this block
-    persistH = await persistNode.open({ keypair: mockKeypair });
-  });
+    persistH = await persistNode.open({ keypair: mockKeypair })
+    // console.log('persistH', persistH.store)
+  })
 
   after(function (done) {
     // runs once after the last test in this block
-    this.timeout(30000); // takes time to close all the connections
+    this.timeout(30000) // takes time to close all the connections
     persistNode
       .close()
       .catch((err) => console.error(err))
-      .then(done);
-  });
+      .then(done)
+  })
 
-  it("should persist on disk", async function () {
-    let mockOb = { text: "saved data " + new Date().toISOString() };
-    persistH.publish(mockOb);
-    this.timeout(5000);
-    const [val] = await once(persistH.beacon, "update");
-    expect(val.text).to.equal(mockOb.text);
-  });
-});
-//process.exit(1);
+  it('should persist on disk', async function () {
+    const mockOb = { text: 'saved data ' + new Date().toISOString() }
+    persistH.publish(mockOb)
+    this.timeout(5000)
+    const [val] = await once(persistH.beacon, 'update')
+    expect(val.text).to.equal(mockOb.text)
+  })
+})
+// process.exit(1);
