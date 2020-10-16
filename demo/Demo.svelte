5d1aa39ddb42bccf2bbb32020938d79f40b37f5c
DougA <douganderson444@gmail.com>
2020-10-16 14:34:20 -0400

    fix demo

--- demo/Demo.svelte
+++ demo/Demo.svelte
@@ -1,65 +1,73 @@
 <script>
   import { onMount, onDestroy } from "svelte";
-  let nameSys;
-  let recent;
-	let publicKey;
-	let contacts = []
   export let name;
 
+  var myNode;
+
+  let recent;
+  let publicKey;
+  let contacts = [];
+
   onMount(() => {
-    setupHypns();
+    const HyPNS = window.hypns;
+    myNode = new HyPNS({ persist: true }); //only one node per site needed
+
+    // Close the SDK connection if the browser is closed
+    window.addEventListener("unload", async (event) => {
+      await myNode.close();
+    });
+
+    setupInstance();
   });
 
   // this function will be called automatically when mounted svelte component is destroyed
-  onDestroy(async () => await nameSys.close());
+  onDestroy(async () => await myNode.close());
 
-  async function setupHypns() {
-    // Close the SDK connection if the browser is closed
-    window.addEventListener("unload", async event => {
-      await nameSys.close();
-    });
+  async function setupInstance(publicKey) {
+    const nameInstance = await myNode.open({ keypair: { publicKey } }); // work with or without a PublicKey
+    contacts = [...contacts, nameInstance];
+    console.log("contacts", contacts);
 
-    const HyPNS = window.hypns;
-    const opts = { persist: true };
-    nameSys = new HyPNS(opts);
-    await nameSys.ready;
-
-    nameSys.beacon.on("update", val => {
+    nameInstance.beacon.on("update", (val) => {
       console.log("beacon update", val);
-      console.log("latest", nameSys.latest.text);
-      recent = nameSys.latest.text;
+      console.log("latest", nameInstance.latest.text);
+      recent = nameInstance.latest.text;
     });
 
-    const retVal = nameSys.publish({ text: "My favorite color is blue" });
+    const retVal = nameInstance.publish({ text: "My favorite color is blue" });
+
+    return nameInstance;
   }
 
-  const handleSubmit = () => {
-    const opts = { keypair: { publicKey }, persist: true };
-    contacts[publicKey] = new HyPNS(opts);
+  const handleSubmit = async () => {
+    const newFriend = await setupInstance(publicKey);
+    contacts = [...contacts, newFriend];
     publicKey = "";
   };
 </script>
 
 <div>
-  <p>Hello {name}</p>
-  {#if nameSys}
-    {#if nameSys.publicKey}
-      Your PublicKey:
-      <br />
-      {nameSys.publicKey}
-    {/if}
-    <p>
-      Latest Value:
-      {#if recent}{recent}{:else}Pending...{/if}
-    </p>
+  {#if contacts && contacts.length > 0 && contacts[0].publicKey}
+    Your PublicKey:
+    <br />
+    {contacts[0].publicKey}
+    <br />
+    Latest Value:<br />
+    {#if recent}{recent}{:else}Pending...{/if}
   {/if}
-  Paste you're friend's publicKey below:
+  <br />
   <br />
   <div>
     <form class="form" on:submit|preventDefault={handleSubmit}>
-      Chat:
+      Paste you're friend's publicKey below:      
       <br />
       <input type="text" bind:value={publicKey} />
     </form>
   </div>
+  <p>List of contacts:</p>
+  <p>
+    {#if contacts && contacts.length > 0}
+      {#each contacts as contact}{contact.publicKey}{/each}
+    {/if}
+  </p>
 </div>
