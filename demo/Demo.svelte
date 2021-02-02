bfa12607777cb3579adbec2f56bff32c5f2a7bd5
Doug <douganderson444@gmail.com>
2021-02-02 15:56:17 -0500

    Demo updates

--- demo/Demo.svelte
+++ demo/Demo.svelte
@@ -3,70 +3,112 @@
 
   var myNode;
 
-  let recent;
-  let publicKey;
-  let contacts = [];
+  export let name;
 
-  onMount(() => {
+  let recent = "";
+  let publicKey = "";
+  let contacts = [];
+  let newFaveColor = "";
+  let lastEntry = "";
+  let myInstance;
+
+  onMount(async () => {
     const HyPNS = window.hypns;
     myNode = new HyPNS({ persist: true }); //only one node per site needed
-
+    const instance = await myNode.open();
+    await instance.ready();
+    myInstance = instance;
     // Close the SDK connection if the browser is closed
     window.addEventListener("unload", async (event) => {
       await myNode.close();
     });
-
-    setupInstance(); // makes a new keyPair since nothin gis passed
   });
 
   // this function will be called automatically when mounted svelte component is destroyed
   onDestroy(async () => await myNode.close());
 
-  async function setupInstance(publicKey) {
-    const nameInstance = await myNode.open({ keypair: { publicKey } }); // work with or without a PublicKey
+  $: !!myInstance ? init() : null;
+
+  const init = async () => {
+    setupInstance(myInstance);
+  };
+
+  const getInstance = async (publicKey) => {
+    try {
+      const inst = await myNode.open({ keypair: { publicKey } }); // works with or without a PublicKey
+      await inst.ready();
+      return inst;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  function setupInstance(nameInstance) {
+    console.log("Setting up", nameInstance.publicKey);
+    const showLatest = (val) => {
+      if (nameInstance.latest && nameInstance.latest.text) {
+        lastEntry = nameInstance.latest.text;
+        console.log("showLatest ", nameInstance.latest.text);
+        recent += `<br/>${nameInstance.publicKey}: ${nameInstance.latest.text}`;
+      }
+    };
+
     contacts = [...contacts, nameInstance];
-    // console.log("contacts", contacts);
-
-    nameInstance.beacon.on("update", (val) => {
-      // console.log("beacon update", val);
-      // console.log("latest", nameInstance.latest.text);
-      recent = nameInstance.latest.text;
-    });
-
-    const retVal = nameInstance.publish({ text: "My favorite color is blue" });
-
-    return nameInstance;
+    console.log("Showing latest ", nameInstance.latest);
+    showLatest(nameInstance.latest);
+    nameInstance.on("update", showLatest);
   }
 
-  const handleSubmit = async () => {
-    const newFriend = await setupInstance(publicKey);
-    contacts = [...contacts, newFriend];
-    publicKey = "";
+  const addPublicKey = async () => {
+    console.log("addPublicKey", publicKey);
+    try {
+      const newFriend = await getInstance(publicKey);
+      setupInstance(newFriend);
+      publicKey = "";
+    } catch (error) {
+      console.error(error);
+    }
   };
+
+  function handleUpdate() {
+    myInstance.publish({ text: newFaveColor });
+    lastEntry = newFaveColor;
+    newFaveColor = "";
+  }
 </script>
 
 <div>
-  {#if contacts && contacts.length > 0 && contacts[0].publicKey}
-    Your PublicKey:
-    <br />
-    {contacts[0].publicKey}
-    <br />
-    Latest Value:<br />
-    {#if recent}{recent}{:else}Pending...{/if}
-  {/if}
-  <br />
-  <br />
+  <p>
+    Hello {name}
+  </p>
   <div>
-    <form class="form" on:submit|preventDefault={handleSubmit}>
-      Paste you're friend's publicKey below:      
+    <form class="form" on:submit|preventDefault={handleUpdate}>
+      Enter anything here to share with your connections:
       <br />
-      <input type="text" bind:value={publicKey} />
+      <input type="text" bind:value={newFaveColor} />
     </form>
   </div>
-  <p>List of contacts:</p>
+  <div>
+    <form class="form" on:submit|preventDefault={addPublicKey}>
+      Paste you're friend's publicKey below to follow updates:
+      <br />
+      <input type="text" bind:value={publicKey} />
+      <br />
+      {publicKey}
+      <br />
+    </form>
+  </div>
+  <br />
+  <p>List of your contacts:</p>
   <p>
     {#if contacts && contacts.length > 0}
-      {#each contacts as contact}{contact.publicKey}{/each}
+      <ul>
+        {#each contacts as contact}
+          <li>{contact.publicKey}</li>
+        {/each}
+      </ul>
     {/if}
   </p>
+  Recent Updates:<br />
+  {@html recent}
 </div>
